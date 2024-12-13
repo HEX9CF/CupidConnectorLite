8b863327a0dba1a02c3392d934b31aac9201521e
HEX9CF <hex9cf@aliyun.com>
2024-12-13 10:46:21 +0800

    Add post request

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"log"
+	"net/http"
+)
 
+func main() {
+	url := "http://a.stu.edu.cn/ac_portal/login.php"
+	username := ""
+	password := ""
+	opr := "pwdLogin"
+	rememberPwd := "0"
+	data := "opr=" + opr + "&userName=" + username + "&pwd=" + password + "&rememberPwd=" + rememberPwd
+	contentType := "application/x-www-form-urlencoded"
+
+	resp, err := http.Post(url, contentType, bytes.NewReader([]byte(data)))
+	if err != nil {
+		log.Println("请求发送失败！")
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	log.Println("请求发送成功")
+	log.Println("登录成功，用户：" + username + "，您已通过上网认证！")
 }
